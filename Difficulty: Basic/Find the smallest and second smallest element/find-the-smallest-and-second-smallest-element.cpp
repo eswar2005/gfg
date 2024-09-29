8b28bbcd699bd1c6ca40157ee01a24d2753f544b
INUMARTI VEERA VENKATA ESWARA KUMAR <131540117+eswar2005@users.noreply.github.com>
2024-09-29 13:03:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find the smallest and second smallest element/find-the-smallest-and-second-smallest-element.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include <limits.h>
+#include <random>
+#include <sstream>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> minAnd2ndMin(vector<int> &arr) {
+        // code here
+        sort(arr.begin(),arr.end());
+        auto it = unique(arr.begin(),arr.end());
+        arr.erase(it,arr.end());
+        if(arr.size()==1){
+            return {-1};
+        }
+        return {arr[0],arr[1]};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        vector<int> ans = ob.minAnd2ndMin(arr);
+        if (ans[0] == -1)
+            cout << -1 << endl;
+        else
+            cout << ans[0] << " " << ans[1] << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
