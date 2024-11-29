1ddbc8a378b256fb0a4d78e5f29f460d2e4a0ee5
INUMARTI VEERA VENKATA ESWARA KUMAR <131540117+eswar2005@users.noreply.github.com>
2024-11-29 15:14:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Palindrome Array/palindrome-array.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    bool isPerfect(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        for(int i=0;i<n/2;i++){
+            if(arr[i]!=arr[n-i-1]){
+                return false;
+                break;
+            }
+        }
+        return true;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+
+        Solution obj;
+        bool res = obj.isPerfect(nums);
+
+        if (res)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+    }
+}
+
+// } Driver Code Ends
