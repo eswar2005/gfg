44276dc36a4102c1baf4de7aa910ef68fcbf52aa
INUMARTI VEERA VENKATA ESWARA KUMAR <131540117+eswar2005@users.noreply.github.com>
2025-06-01 11:35:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Insert in Middle of Linked List/insert-in-middle-of-linked-list.cpp
@@ -0,0 +1,55 @@
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+// Function to insert a node in the middle of the linked list.
+class Solution {
+  public:
+    Node *insertInMiddle(Node *head, int x) {
+        // Code Here
+        Node* newNode = new Node(x);
+        if (head == NULL) {
+            // If list is empty, new node becomes head
+            return newNode;
+        }
+        Node* temp=head;
+        int cnt=0;
+        while(temp){
+            cnt++;
+            temp=temp->next;
+        }
+        temp=head;
+        int mid=ceil(cnt/2);
+        //cout<<mid<<"\n";
+        // if (mid == 0) {
+        //     newNode->next = head;
+        //     return newNode;
+        // }
+        
+       if(cnt%2==0){
+           mid=mid+1;
+       }
+        else{
+            mid=mid+2;
+        }
+        Node* p1=head;
+        Node* p2=NULL;
+        mid--;
+        while(mid>1){
+            mid--;
+            p1=p1->next;
+        }
+        p2=p1->next;
+        p1->next=newNode;
+        newNode->next=p2;
+        
+        return head;
+    }
+};
